a19431c8aae0fc623325c81bb0f2dd292a579352
mecha-aima <aimo.malik77@gmail.com>
2026-02-20 14:24:09 +0500

    Updated training script on feature-ml

--- src/train.py
+++ src/train.py
@@ -5,9 +5,10 @@ import joblib, os
 X_train = pd.read_csv('data/processed/X_train.csv')
 y_train = pd.read_csv('data/processed/y_train.csv').squeeze()
 
-model = LogisticRegression(max_iter=200)
+model = LogisticRegression(max_iter=300)
 model.fit(X_train, y_train)
 
 os.makedirs('models', exist_ok=True)
 joblib.dump(model, 'models/model.pkl')
 print("Model trained and saved to models/model.pkl")
+print("Changed line.")
